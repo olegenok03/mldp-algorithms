c9d3df292a5c238681292fb48efb60986e4c647a
Chris Harper <chris-harper@inbox.ru>
2026-09-23 00:37:56 +0300

    Fixed palindrome issue with number 999...999 (1500 digits); Corrected readme_palindrome

--- hw1-algorithmic_complexity/task1-palindrome/palindrome.py
+++ hw1-algorithmic_complexity/task1-palindrome/palindrome.py
@@ -1,8 +1,8 @@
-from math import log10
-
-
 def is_palindrome(a: int) -> bool:
-    ord_num = int(log10(a)) + 1
+    ord_num_exp, ord_num = 1, 0
+    while ord_num_exp <= a:
+        ord_num_exp *= 10
+        ord_num += 1
     for i in range(ord_num // 2):
         if (a // 10 ** i) % 10 != (a // 10 ** (ord_num - i - 1)) % 10:
             return False
